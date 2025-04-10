274fb4f789757987df0eadeb4062aac23526b151
Furkan Baltacı <hasanfurkanbaltaci2@gmail.com>
2025-04-10 22:58:09 +0300

    Catalog Application Use Case Development with CQRS and MediatR

---
+++ src/Modules/Catalog/Catalog/Products/Features/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace Catalog.Products.Features.GetProductById
+{
+    public record GetProductByIdQuery(Guid ProductId)
+        : IQuery<GetProductByIdResult>;
+
+    public record GetProductByIdResult(ProductDto Product);
+
+    internal class GetProductByIdHandler(CatalogDbContext dbContext)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            var product = await dbContext.Products
+                .AsNoTracking()
+                .SingleOrDefaultAsync(p => p.Id == query.ProductId, cancellationToken) 
+                ?? throw new Exception($"Product not found: {query.ProductId}");
+
+            var productDto = product.Adapt<ProductDto>();
+
+            return new GetProductByIdResult(productDto);
+        }
+    }
+}
